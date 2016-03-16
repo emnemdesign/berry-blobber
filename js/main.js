3bbc05f538b9e39306e6af8a5525253148b6d867
mcle0341 <mcle0341@algonquinlive.com>
2016-03-16 08:18:31 -0400

    added variable for write berries

--- js/main.js
+++ js/main.js
@@ -34,7 +34,37 @@ var redCurrant = {
 // ##########################################################
 
 
-// Write your function here, name it `writeBerries`
+var writeBerries = function (berries) {
+  document.write('<h1>Berries</h1>');
+
+
+  berries.forEach(function (berry) {
+    document.write('<h2>Blueberries</h2>');
+    document.write('<dl>');
+
+    document.write('<dt>Grows on a shrub?</dt>');
+    if (berry.onShrub) {
+      document.write('<dd>yes</dd>')
+    } else {
+      document.write('<dd>no</dd>')
+    };
+    document.write('<dt>Energy</dt>')
+    document.write('<dd>' + berry.energy + 'kJ</dd>');
+
+    document.write('<dt>Carbohydrates</dt>')
+    document.write('<dd>' + berry.carbs + 'g</dd>');
+
+    document.write('<dt>Protein</dt>')
+    document.write('<dd>' + berry.protein + 'g</dd>');
+
+
+
+
+    document.write('</dl>');
+  });
+
+}
+
 
 
 // ##########################################################
